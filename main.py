d1d91832ba829e7cc7173ee47883f5563ad77c63
starg09 <ultraguille2006@gmail.com>
2015-06-19 12:11:08 -0300

    Corrección de algunos datos
    Debe abrirse con Encoding DOS (CP 437)
    
    Avances en el menu, mensaje de error agregado.

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ def printlogo():
 
 ===============================================================================
 
-                _____               _      
-               |  __ \\             | |     
-    _ __  _   _| |__) | __ ___   __| | ___ 
-   | \'_ \\| | | |  ___/ \'__/ _ \\ / _` |/ _ \\
-   | |_) | |_| | |   | | | (_) | (_| |  __/
-   | .__/ \\__, |_|   |_|  \\___/ \\__,_|\\___|
-   | |     __/ |                           
-   |_|    |___/                            
-                                            
+               _____               _      
+              |  __ \\             | |     
+   _ __  _   _| |__) | __ ___   __| | ___ 
+  | \'_ \\| | | |  ___/ \'__/ _ \\ / _` |/ _ \\
+  | |_) | |_| | |   | | | (_) | (_| |  __/
+  | .__/ \\__, |_|   |_|  \\___/ \\__,_|\\___|
+  | |     __/ |                           
+  |_|    |___/                            
+
 
 ===============================================================================
 """
@@ -31,9 +31,9 @@ def printlogo():
 
 	for linea in logo.split("\n"):
 		print linea
-		time.sleep(0.06)
+		time.sleep(0.05)
 	time.sleep(1)
-	print Style.BRIGHT + "    Presione Enter para continuar..." + Style.RESET_ALL,
+	print Style.BRIGHT + "    Presionea Enter para continuar..." + Style.RESET_ALL,
 	raw_input() #Seteo a una variable para que no salga en pantalla
 
 def menuprincipal():
@@ -44,46 +44,49 @@ def printlogo_mini():
 ===============================================================================
    ÚÄ¿Â ÂÉÍ»ÂÄ¿ÚÄ¿ÚÂ¿ÚÄ¿
    ÃÄÙÀÂÙÌÍ¼ÃÂÙ³ ³ ³³Ã´ 
-   Á   Á Ê  ÁÀÄÀÄÙÄÁÙÀÄÙ  
+   Á   Á Ê  ÁÀÄÀÄÙÄÁÙÀÄÙ   
 ===============================================================================
 
 """ + Fore.RESET
 
 def menuprincipal():
 	continuar = False
+	OpcInv = False
 	while not(continuar):
 		limpiar_pantalla()
 		printlogo_mini()
 		print """
-  ­Bienvenido a pyProde!
-  Por favor, ingrese el n£mero correspondiente a la opci¢n deseada.
+""" + Back.WHITE + Fore.BLACK + """  ­Bienvenido a pyProde!  
+  Por favor, ingrese el n£mero correspondiente a la opci¢n deseada.  """ + Back.RESET + Fore.RESET + """
 
-    1: Ingresar como Administrador
-    2: Ingresar como Jugador
-    0: Salir
-
-
-  """
-  		try:
-  			opcion = int(raw_input("Ingrese la opci¢n elegida, y luego presione Enter: "))
-  			OpcInv = True if opcion in [0,1,2] else False
-  			continuar = True if opcion in [0,1,2] else False
-  			if opcion == 1:
-  				menuAdmin()
-  			elif opcion == 2:
-  				loginJugador()
-  			elif opcion == 0:
-  				print """
-
-
-­Adios!
-
-
-"""
-				time.sleep(2)
+	1: Ingresar como Administrador
+	2: Ingresar como Jugador
+	0: Salir"""
+		print ""
+		if OpcInv:
+			print Back.RED + Fore.WHITE + Style.BRIGHT + "  Error: Ha ingresado una opci¢n inv lida. Por favor, int‚ntelo de nuevo...  " + Back.RESET + Fore.RESET + Style.RESET_ALL
+		else:
+			print ""
+		print ""
+		try:
+			opcion = int(raw_input("  Ingrese la opci¢n elegida, y luego presione Enter: "))
+			OpcInv = False if opcion in [0,1,2] else True
+			continuar = True if opcion in [0,1,2] else False
+			if opcion == 1:
+				menuAdmin()
+			elif opcion == 2:
+				loginJugador()
+			elif opcion == 0:
+				print Back.GREEN + Fore.BLACK + """
+               
+    ­Adi¢s!    
+               
+""" + Back.RESET + Fore.RESET
+				time.sleep(1.5)
+				limpiar_pantalla()
 		except ValueError:
-  			OpcInv = True
-  			
+			OpcInv = True
+				
 
 
 
