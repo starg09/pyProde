1ea35928d4768b9922f341f073257902f819cddd
starg09 <ultraguille2006@gmail.com>
2015-06-20 23:28:40 -0300

    Terminado menú Admin
    * Agregada funcion "s/n"
    * Corregidos algunos strings multi-linea por comodidad al editar
    * Agregado "Listar Partidos"
    * Agregado "Agregar Resultado" (Admin)
    * Agregado "Listar Usuarios"
    * Agregado "Agregar Usuario"
    * Terminado "Cargar Fixture"
    * Activadas las funciones correspondientes en el menu de Admin.

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def ordenar_fixture(lista_puntajes):
 	return lista_ordenada
 
 def preparar_archivos():
+	"""De no existir, crea los archivos donde se almacenan los datos de los usuarios, y el fixture.
+	Se usa al comienzo del programa
+	"""
 	if not(os.path.isfile("usuarios.dat")):
 		arch = open("usuarios.dat", "wb")
 		arch.close()
@@ -49,8 +52,13 @@ def printlogo():
 	print Style.BRIGHT + "    Presiona Enter para continuar..." + Style.RESET_ALL,
 	raw_input() #Seteo a una variable para que no salga en pantalla
 
-def menuprincipal():
-	limpiar_pantalla()
+def opcionsn(linea):
+	s = "k"
+	while s not in ["S","s","N","n"]:
+		s = raw_input(linea + "[sS/nN]: ")
+		if s not in ["S","s","N","n"]:
+			print Back.RED + Fore.WHITE + Style.BRIGHT + "\n  Opci¢n invalida. Ingrese \"s/S\" para s¡, o \"n/N\" para no. Luego presione Enter.\n" + Back.RESET + Fore.RESET + Style.RESET_ALL
+	return True if s in ["S","s"] else False
 
 def printlogo_mini():
 	print Fore.GREEN + """
@@ -68,8 +76,7 @@ def menuJugador(nombre):
 	while not(salir):
 		limpiar_pantalla()
 		printlogo_mini()
-		print """
-""" + Back.WHITE + Fore.BLACK + "  Menu de Jugador - " + nombre + "  " + Back.RESET + Fore.RESET + """
+		print "\n" + Back.WHITE + Fore.BLACK + "  Menu de Jugador - " + nombre + "  " + Back.RESET + Fore.RESET + """
 
     1: Mi Prode
     2: Modificar Partido
@@ -113,36 +120,133 @@ def ListarFixture():
 	ArchFixture = open("fixture.dat","rb")
 	fixture, dummy = leer_desde_archivo(ArchFixture)
 	ArchFixture.close()
-	fixture = ordenar_fixture(fixture)
-	print "\n  Lista de Partidos  \n"
-	i = 1
-	for item in fixture:
-		print "  " + str(item["numero_partido"]) + ") " + str(item["local"]) + " - " + str(item ["visitante"]),
-		if not(item["jugado"]):
-			print " (" + str(item["goles_local"]) + "-" + str(item["goles_visitante"]) + ")"
+	if not(fixture):
+		print "\n  Error: Fixture no cargado.\n"
+	else:
+		fixture = ordenar_fixture(fixture)
+		print "\n  Lista de Partidos  \n"
+		i = 1
+		for item in fixture:
+			print "  " + str(item["numero_partido"]) + ") " + str(item["local"]) + " - " + str(item ["visitante"]),
+			if (item["jugado"]):
+				print " (" + str(item["goles_local"]) + "-" + str(item["goles_visitante"]) + ")"
+			else:
+				print ""
+			i += 1
+			if i%7==1 and i<len(fixture):
+				cualquiera = raw_input("\n\n  Presione enter para la siguiente pagina")
+				limpiar_pantalla()
+				print "\n  Lista de Partidos  \n"
+	terminar = raw_input("\n\n  Presione Enter para volver al men£ anterior...")
+
+def AgregarResultado():
+	limpiar_pantalla()
+	ArchFixture = open("fixture.dat","rb")
+	fixture, dummy = leer_desde_archivo(ArchFixture) #Variable dummy se usar  siempre cuando no se necesite esa var
+	ArchFixture.close()
+	if not(fixture):
+		print "\n  Antes de agregar un resultado, debe cargarse el fixture.\n"
+		terminar = raw_input("\n  Presione Enter para volver al men£ anterior...")
+	else:
+		fixture = ordenar_fixture(fixture)
+		ids_validos = []
+		for item in fixture:
+			ids_validos.append(item["numero_partido"])
+		continuar_id = True
+		while continuar_id:
+			id_partido = -999
+			while id_partido not in ids_validos and continuar_id:
+				id_partido = leer_natural("  Inserte su numero de partido, del 1 al " + str(max(ids_validos)) + ": ")
+				if id_partido not in ids_validos:
+					continuar_id = opcionsn("  N£mero de partido fuera de rango. ¨Desea ingresar uno nuevamente?")
+			if id_partido in ids_validos:
+				modificar_res = False
+				while not(modificar_res):
+					print "\n  Modificando partido " + str(id_partido) + ": " + fixture[id_partido-1]["local"] + " - " + fixture[id_partido-1]["visitante"] + "\n"
+					gol_l = leer_natural(str(fixture[id_partido-1]["local"]) + ": ")
+					gol_v = leer_natural(str(fixture[id_partido-1]["visitante"]) + ": ")
+					modificar_res = opcionsn("  Usted ingres¢:" + fixture[id_partido-1]["local"] + " " + str(gol_l) + " - " + fixture[id_partido-1]["visitante"] + " " + str(gol_v) + "\n  ¨Es esto correcto?")
+				fixture[id_partido-1]["goles_local"] = gol_l
+				fixture[id_partido-1]["goles_visitante"] = gol_v
+				fixture[id_partido-1]["jugado"] = True
+				print "\n  Resultado guardado con ‚xito."
+				continuar_id = opcionsn("  ¨Desea agregar otro resultado?")
+
+def ListarUsuarios():
+	limpiar_pantalla()
+	ArchUsuarios = open("usuarios.dat","rb")
+	usuarios, dummy = leer_desde_archivo(ArchUsuarios)
+	ArchUsuarios.close()
+	if not(usuarios):
+		print "\n  No hay usuarios para listar.\n"
+	else:
+		usuarios_ordenados = sorted(usuarios, key=lambda x: (x['nombre']))
+		print "\n  Lista de Usuarios  \n"
+		i = 1
+		for item in usuarios:
+			print "  * " + item["nombre"]
+			i += 1
+			if i%8==1 and i<len(usuarios):
+				cualquiera = raw_input("\n\n  Presione enter para la siguiente pagina")
+				limpiar_pantalla()
+				print "\n  Lista de Usuarios  \n"
+	terminar = raw_input("\n  Presione Enter para volver al men£ anterior...")
+
+def AgregarUsuario():
+	limpiar_pantalla()
+	ArchFixture = open("fixture.dat","rb")
+	fixture, dummy = leer_desde_archivo(ArchFixture) #Variable dummy se usar  siempre cuando no se necesite esa var
+	ArchFixture.close()
+	if not(fixture):
+		print "\n  Antes de crear usuarios, debe cargarse el fixture.\n"
+	else:
+		esalfan = False
+		while not(esalfan):
+			nombre_elegido = raw_input("\n  [Los nombres deben tener solo caracteres alfanum‚ricos]\n  [La ¥ no est  permitida]\n  Por favor, ingrese un nuevo nombre de usuario: ")
+			esalfan = nombre_elegido.isalnum()
+			if not(nombre_elegido.isalnum()):
+				limpiar_pantalla()
+				print Back.RED + Fore.WHITE + Style.BRIGHT + "\n  Error: Solo puede ingresar caracteres alfanum‚ricos.\n" + Back.RESET + Fore.RESET + Style.RESET_ALL
+
+		nombre_elegido = nombre_elegido.upper()
+		ArchUsuarios = open("usuarios.dat","rb")
+		usuarios, dummy = leer_desde_archivo(ArchUsuarios)
+		ArchUsuarios.close()
+		NombreExistente = False
+		if not(usuarios):
+			usuarios = []
+		for item in usuarios:
+			if (item["nombre"] == nombre_elegido):
+					NombreExistente = True
+		if NombreExistente:
+			print Back.RED + Fore.WHITE + Style.BRIGHT + "\n  Error: El usuario " + nombre_elegido + " ya existe en el sistema.\n" + Back.RESET + Fore.RESET + Style.RESET_ALL
 		else:
-			print ""
-		i += 1
-		if i%7==1 and i<len(fixture):
-			cualquiera = raw_input("\n\n  Presione enter para la siguiente pagina")
-			limpiar_pantalla()
-			print "\n  Lista de Partidos  \n"
-	terminar = raw_input("\n\n  Presione enter para volver al menu anterior...")
+			nuevo_usuario = {
+				"nombre": nombre_elegido,
+				"puntaje": 0,
+				"prode": []
+			}
+			for partido in fixture:
+				nuevo_usuario["prode"].append({
+						"numero_partido": partido["numero_partido"],
+						"goles_local": 0,
+						"goles_visitante": 0
+				})
+			usuarios.append(nuevo_usuario)
+			ArchUsuarios = open("usuarios.dat","wb")
+			guardar_en_archivo(ArchUsuarios, usuarios)
+			ArchUsuarios.close()
+			print "\n  Usuario creado: " + nombre_elegido.upper()
+	terminar = raw_input("  Presione Enter para volver al men£ anterior...")
+
 def CargarFixture():
 	limpiar_pantalla()
 	ArchUsuarios = open("usuarios.dat","rb")
-	usuarios = []
-	fin_lectura_usuarios = False
-	while not(fin_lectura_usuarios):
-		item, fin_lectura_usuarios = leer_desde_archivo(ArchUsuarios)
-		if item:
-			usuarios.append(item)
+	usuarios, dummy = leer_desde_archivo(ArchUsuarios)
 	ArchUsuarios.close()
-	if len(usuarios)>0:
+	if usuarios:
 		print "\n  Ya se han creado usuarios, no se puede volver a cargar el fixture.\n"
-		terminar = raw_input("  Presione enter para volver al menu anterior...")
 	else:
-		i = 1
 		finarch = False
 		ImpFixture = open("fxt_ca_2015.txt","rt")
 		lista = importar_fixture(ImpFixture)
@@ -150,13 +254,8 @@ def CargarFixture():
 		ArchFixture = open("fixture.dat","wb")
 		guardar_en_archivo(ArchFixture, lista)
 		ArchFixture.close()
-		print "\n  Fixture cargado con ‚xito.\n"
-		terminar = raw_input("  Presione enter para volver al menu anterior...")
-
-
-
-
-
+		print "\n  Fixture cargado con ‚xito. " + str(len(lista)) + " partidos detectados.\n"
+	terminar = raw_input("  Presione Enter para volver al men£ anterior...")
 
 def menuAdmin():
 	salir = False
@@ -164,8 +263,7 @@ def menuAdmin():
 	while not(salir):
 		limpiar_pantalla()
 		printlogo_mini()
-		print """
-""" + Back.WHITE + Fore.BLACK + """  Menu de Administrador  """ + Back.RESET + Fore.RESET + """
+		print "\n" + Back.WHITE + Fore.BLACK + """  Menu de Administrador  """ + Back.RESET + Fore.RESET + """
 
     1: Cargar fixture del torneo
     2: Agregar usuarios
@@ -185,14 +283,11 @@ def menuAdmin():
 			if opcion == 1:
 				CargarFixture()
 			elif opcion == 2:
-				#TO-DO
-				pass
+				AgregarUsuario()
 			elif opcion == 3:
-				#TO-DO
-				pass
+				ListarUsuarios()
 			elif opcion == 4:
-				#TO-DO
-				pass
+				AgregarResultado()
 			elif opcion == 5:
 				ListarFixture()
 			elif opcion == 0:
@@ -206,8 +301,7 @@ def menuprincipal():
 	while continuar:
 		limpiar_pantalla()
 		printlogo_mini()
-		print """
-""" + Back.WHITE + Fore.BLACK + """  ­Bienvenido a pyProde!  
+		print "\n" + Back.WHITE + Fore.BLACK + """  ­Bienvenido a pyProde!  
   Por favor, ingrese el n£mero correspondiente a la opci¢n deseada.  """ + Back.RESET + Fore.RESET + """
 
 	1: Ingresar como Administrador
@@ -227,11 +321,7 @@ def menuprincipal():
 			elif opcion == 2:
 				loginJugador()
 			elif opcion == 0:
-				print Back.GREEN + Fore.BLACK + """
-               
-    ­Adi¢s!    
-               
-""" + Back.RESET + Fore.RESET
+				print Back.GREEN + Fore.BLACK + "\n               \n    ­Adi¢s!    \n               \n" + Back.RESET + Fore.RESET
 				time.sleep(1.5)
 				limpiar_pantalla()
 				continuar = False
