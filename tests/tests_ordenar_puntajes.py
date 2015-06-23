798f996b03dc320d03d4a98ac137a35a130b3441
starg09 <ultraguille2006@gmail.com>
2015-06-23 10:49:31 -0300

    Corregido test completamente
    Faltaba corregir la clave en los casos a comparar

--- tests/tests_ordenar_puntajes.py
+++ tests/tests_ordenar_puntajes.py
@@ -20,14 +20,14 @@ class TestOrdenarPuntajes(unittest.TestCase):
 	def test_ordenar_puntajes_con_una_lista_ordenada_por_puntajes_no_modifica_nada(self):
 		# Setup
 		lista_puntaje = [
-			{'usuario': 'usuario1', 'puntaje': 5}, 
-			{'usuario': 'usuario2', 'puntaje': 3}, 
-			{'usuario': 'usuario3', 'puntaje': 1}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 3}, 
+			{'nombre': 'usuario3', 'puntaje': 1}, 
 		]
 		resultado_esperado = [
-			{'usuario': 'usuario1', 'puntaje': 5}, 
-			{'usuario': 'usuario2', 'puntaje': 3}, 
-			{'usuario': 'usuario3', 'puntaje': 1}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 3}, 
+			{'nombre': 'usuario3', 'puntaje': 1}, 
 		]
 
 		# Exercise
@@ -39,14 +39,14 @@ class TestOrdenarPuntajes(unittest.TestCase):
 	def test_ordenar_puntajes_con_una_lista_ordenada_por_usuarios_no_modifica_nada(self):
 		# Setup
 		lista_puntaje = [
-			{'usuario': 'usuario1', 'puntaje': 5}, 
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario3', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario3', 'puntaje': 5}, 
 		]
 		resultado_esperado = [
-			{'usuario': 'usuario1', 'puntaje': 5}, 
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario3', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario3', 'puntaje': 5}, 
 		]
 
 		# Exercise
@@ -58,14 +58,14 @@ class TestOrdenarPuntajes(unittest.TestCase):
 	def test_ordenar_puntajes_con_una_lista_ordenada_por_puntajes_y_usuarios_no_modifica_nada(self):
 		# Setup
 		lista_puntaje = [
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario1', 'puntaje': 3}, 
-			{'usuario': 'usuario3', 'puntaje': 3}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 3}, 
+			{'nombre': 'usuario3', 'puntaje': 3}, 
 		]
 		resultado_esperado = [
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario1', 'puntaje': 3}, 
-			{'usuario': 'usuario3', 'puntaje': 3}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 3}, 
+			{'nombre': 'usuario3', 'puntaje': 3}, 
 		]
 
 		# Exercise
@@ -78,14 +78,14 @@ class TestOrdenarPuntajes(unittest.TestCase):
 	def test_ordenar_puntajes_con_una_lista_desordenada_por_usuarios_retorna_lista_ordenada(self):
 		# Setup
 		lista_puntaje = [
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario3', 'puntaje': 5}, 
-			{'usuario': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario3', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
 		]
 		resultado_esperado = [
-			{'usuario': 'usuario1', 'puntaje': 5}, 
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario3', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 5}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario3', 'puntaje': 5}, 
 		]
 
 		# Exercise
@@ -97,14 +97,14 @@ class TestOrdenarPuntajes(unittest.TestCase):
 	def test_ordenar_puntajes_con_una_lista_desordenada_por_puntajes_y_usuarios_retorna_lista_ordenada(self):
 		# Setup
 		lista_puntaje = [
-			{'usuario': 'usuario1', 'puntaje': 3}, 
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario3', 'puntaje': 3}, 
+			{'nombre': 'usuario1', 'puntaje': 3}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario3', 'puntaje': 3}, 
 		]
 		resultado_esperado = [
-			{'usuario': 'usuario2', 'puntaje': 5}, 
-			{'usuario': 'usuario1', 'puntaje': 3}, 
-			{'usuario': 'usuario3', 'puntaje': 3}, 
+			{'nombre': 'usuario2', 'puntaje': 5}, 
+			{'nombre': 'usuario1', 'puntaje': 3}, 
+			{'nombre': 'usuario3', 'puntaje': 3}, 
 		]
 
 		# Exercise
